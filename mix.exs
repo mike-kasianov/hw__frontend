f3158861c7e9d7db80242aa4d2844705840c8d0b
Michael Kasianov <kasianov.michael@gmail.com>
2019-07-04 18:46:37 +0000

    Freeze deps versions

--- mix.exs
+++ mix.exs
@@ -32,14 +32,13 @@ defmodule Frontend.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.6"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:distillery, "~> 2.0"}
+      {:phoenix, "1.4.9"},
+      {:phoenix_pubsub, "1.1.2"},
+      {:phoenix_html, "2.13.3"},
+      {:phoenix_live_reload, "1.2.1", only: :dev},
+      {:gettext, "0.17.0"},
+      {:jason, "1.1.2"},
+      {:plug_cowboy, "2.1.0"}
     ]
   end
 end
